f81a48a434ea9108c2a1838f15da9e453ae59ca8
Ubuntu <ubuntu@teste.5q3kd3htqejuxgaojaz05gssbd.nx.internal.cloudapp.net>
2021-06-05 18:34:40 +0000

    Adding network_pkt_recv

--- extractor.py
+++ extractor.py
@@ -1,11 +1,12 @@
-import psutil
 import csv
 import sys
+import datetime as dt
+import psutil
 
 experiment_number = sys.argv[1]
-iteration_times = int(sys.argv[2])
 
-fields = ["cpu_usage_percent",
+fields = ["time",
+          "cpu_usage_percent",
           "cpu_freq_current",
           "memory_total",
           "memory_available",
@@ -14,14 +15,14 @@ fields = ["cpu_usage_percent",
           "memory_free",
           "swap_used",
           "swap_percent",
+          "packets_received",
           "cpu_temperature"]
 
 with open('run_'+str(experiment_number)+'.csv', 'w') as f:
     write = csv.writer(f)
     write.writerow(fields)
 
-    i = 0
-    while i < iteration_times:
+    while True:
 
         cpu_usage_percent = psutil.cpu_percent(interval=1, percpu=False)
         cpu_freq_current = psutil.cpu_freq().current
@@ -32,15 +33,19 @@ with open('run_'+str(experiment_number)+'.csv', 'w') as f:
         memory_free = psutil.virtual_memory().free
         swap_used = psutil.swap_memory().used
         swap_percent = psutil.swap_memory().percent
-
+        network_pkt_recv = psutil.net_io_counters(pernic=True)
+        network_pkt_recv = network_pkt_recv['eth0'][3]
         cpu_temperature = psutil.sensors_temperatures()
         for entry in cpu_temperature['cpu_thermal']:
             cpu_temperature = entry.current
 
 
-        rows = [[cpu_usage_percent, cpu_freq_current, memory_total, memory_available,
-                 memory_percent, memory_used, memory_free, swap_used, swap_percent, cpu_temperature]]
+
+        rows = [[dt.datetime.now().time(),cpu_usage_percent, 
+                 cpu_freq_current, memory_total, memory_available,
+                 memory_percent, memory_used, memory_free, swap_used, 
+                 swap_percent, network_pkt_recv,cpu_temperature]]
 
         write.writerows(rows)
-        i = i + 1
+
 
