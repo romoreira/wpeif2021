fab536d6000f9ce3950c971177b711bc94f524e4
Ubuntu <ubuntu@test.gsb3o0a0ehfujaq0cd2gnxdmmd.nx.internal.cloudapp.net>
2021-06-06 17:36:52 +0000

    Automating MQTT Latency

--- README.md
+++ README.md
@@ -2,5 +2,26 @@
 * Extrator de metricas
 * Experimentos
 
+-- Installing MQTT Brocker
+sudo apt-get install mosquitto
+
+-- Configuring IP and Port for MQTT Broker
+sudo vim /etc/mosquitto/mosquitto.conf
+
+Insert those lines in mosquitto.conf
+** bind_address 10.0.0.4
+** allow_anonymous true
+Resetar Mosquitto Service
+sudo service mosquitto restart
+
+
+-- Installing required Packages
+* sudo apt-get install python3-pip
+* sudo apt-get install golang
+
+-- Intalling MQTT Bench
+go get github.com/krylovsk/mqtt-benchmark
+
 -- Installing PSUTIL
-* pip3 install psutil
+* pip3 install psutil numpu
+
--- mqtt_metrics.py
+++ mqtt_metrics.py
@@ -1,11 +1,19 @@
+import sys
 import json
 import os
+import subprocess
 
 # Opening JSON file
 
-stream = os.popen('/home/ubuntu/go/bin/mqtt-benchmark --broker tcp://10.0.0.4:1883 --count 100 --size 100 --clients 100 --qos 2 --format json --quiet')
-output = stream.readlines()
 
-print(output)
-data = json.load(output)
-print(data['totals']['msg_time_mean_avg']) 
+messages_amount = sys.argv[1]
+message_size = sys.argv[2]
+client_amount = sys.argv[3]
+qos_spec = sys.argv[4]
+
+json_output = subprocess.check_output("/home/ubuntu/go/bin/mqtt-benchmark --broker tcp://10.0.0.4:1883 --count "+str(messages_amount)+" --size "+str(message_size)+" --clients "+str(client_amount)+" --qos "+str(qos_spec)+" --format json --quiet", shell=True).strip()
+
+json_output = json_output.decode()
+json_output = json.loads(json_output)
+
+print(json_output['totals']['msg_time_mean_avg']) 
