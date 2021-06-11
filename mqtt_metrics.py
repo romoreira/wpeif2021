6ef72a0c8e1e8777ae86f236327343ef24b501f8
rodrigo <rodrigo.moriera206@gmail.com>
2021-06-11 16:57:19 -0300

    Ready to run experiments

--- mqtt_metrics.py
+++ mqtt_metrics.py
@@ -19,14 +19,14 @@ experiment_number = sys.argv[5]
 experiment_times = sys.argv[6]
 experiment_times = int(experiment_times)
 
-with open('mqtt_latency_'+str(experiment_number)+'.csv', 'w') as f:
+with open('mqtt_messages_amount'+str(messages_amount)+'_message_size'+str(message_size)+'_client_amount'+str(client_amount)+'_qos_spec'+str(qos_spec)+'_experiment_numer'+str(experiment_number)+'_'+str(experiment_number)+'.csv', 'w') as f:
 
     write = csv.writer(f)
     write.writerow(fields)
 
     while experiment_times > 0:
 
-        json_output = subprocess.check_output("/home/ubuntu/go/bin/mqtt-benchmark --broker tcp://10.0.0.4:1883 --count "+str(messages_amount)+" --size "+str(message_size)+" --clients "+str(client_amount)+" --qos "+str(qos_spec)+" --format json --quiet", shell=True).strip()
+        json_output = subprocess.check_output("/home/rodrigo/go/bin/mqtt-benchmark --broker tcp://192.168.0.2:1883 --count "+str(messages_amount)+" --size "+str(message_size)+" --clients "+str(client_amount)+" --qos "+str(qos_spec)+" --format json --quiet", shell=True).strip()
 
         json_output = json_output.decode()
         json_output = json.loads(json_output)
@@ -38,5 +38,7 @@ with open('mqtt_latency_'+str(experiment_number)+'.csv', 'w') as f:
         rows = [[dt.datetime.now().time(),msg_time_mean_avg]]
         write.writerows(rows)
 
-        print(experiment_times)
+        #print(experiment_times)
         experiment_times = experiment_times - 1
+
+    print("Finished!")
